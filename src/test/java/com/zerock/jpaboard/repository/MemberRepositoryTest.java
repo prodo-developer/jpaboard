e1ee79013feadcfaf73b4afc67247056abe9c7ae
prodo-developer <wjdals300@gmail.com>
2021-05-20 20:21:16 +0900

    TEST : 회원 테스트

---
+++ src/test/java/com/zerock/jpaboard/repository/MemberRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.zerock.jpaboard.repository;
+
+import com.zerock.jpaboard.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원 데이터 100명 만들기")
+    public void insertMembers() {
+        IntStream.rangeClosed(1,100).forEach(i -> {
+
+            Member member = Member.builder()
+                    .email("user" + i + "@aaa.com")
+                    .password("1111")
+                    .name("USER" + i)
+                    .build();
+
+            memberRepository.save(member);
+        });
+    }
+}
