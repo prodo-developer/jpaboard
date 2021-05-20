adc4f4d6e958b0f72be36b8e027775e570c4cc09
prodo-developer <wjdals300@gmail.com>
2021-05-20 20:19:05 +0900

    ADD : Board 엔티티 추가

---
+++ src/main/java/com/zerock/jpaboard/entity/Board.java
@@ -0,0 +1,34 @@
+package com.zerock.jpaboard.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "writer") // @ToString는 항상 exclude 합니다. 지연로딩 사용시 반드시 선언
+public class Board extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+//    @ManyToOne // 다대일 디폴트 (FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY) // 다대일 명시적으로 지연 로딩 지정
+    private Member writer; // 연관관계 지정
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+}
