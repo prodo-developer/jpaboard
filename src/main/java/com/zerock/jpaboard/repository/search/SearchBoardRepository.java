9058d0c7e2390cc6a605b7a66052dd77412bd9ec
prodo-developer <wjdals300@gmail.com>
2021-08-13 01:53:31 +0900

    feat : QuerydslRepositorySupport 활용

---
+++ src/main/java/com/zerock/jpaboard/repository/search/SearchBoardRepository.java
@@ -0,0 +1,13 @@
+package com.zerock.jpaboard.repository.search;
+
+import com.zerock.jpaboard.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface SearchBoardRepository {
+
+    Board search1();
+
+    Page<Object[]> searchPage(String type, String keyword, Pageable pageable);
+
+}
---
+++ src/main/java/com/zerock/jpaboard/repository/search/SearchBoardRepositoryImpl.java
@@ -0,0 +1,142 @@
+package com.zerock.jpaboard.repository.search;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.JPQLQuery;
+import com.zerock.jpaboard.entity.Board;
+import com.zerock.jpaboard.entity.QBoard;
+import com.zerock.jpaboard.entity.QMember;
+import com.zerock.jpaboard.entity.QReply;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 쿼리 메서드나 @Query 등으로 처리할 수 없는 기능은 별도의 인터페이스로 설계
+ * 별도의 인터페이스에 대한 구현 클래스를 작성합니다. QuerydslRepositorySupport라는 클래스를 부모 클래스로 사용
+ * 구현 클래스에 인터페이스의 기능을 Q도메인 클래스와 JPQLQuery를 이용해서 구현
+ */
+@Log4j2
+public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
+
+    public SearchBoardRepositoryImpl() {
+        super(Board.class);
+    }
+
+    @Override
+    public Board search1() {
+
+        log.info("search1........................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+//        jpqlQuery.select(board.bno.eq(1L));
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
+        log.info("---------------------------");
+        log.info(tuple);
+        log.info("---------------------------");
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        return null;
+    }
+
+    @Override
+    public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
+
+        log.info("searchPage.............................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        //SELECT b, w, count(r) FROM Board b
+        //LEFT JOIN b.writer w LEFT JOIN Reply r ON r.board = b
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        // 검색조건 처리의 BooleanExpression
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if(type != null){
+            String[] typeArr = type.split("");
+            //검색 조건을 작성하기
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for (String t:typeArr) {
+                switch (t){
+                    case "t":
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        //order by
+        Sort sort = pageable.getSort();
+
+        //tuple.orderBy(board.bno.desc());
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending()? Order.ASC: Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class, "board");
+            tuple.orderBy(new OrderSpecifier(direction, orderByExpression.get(prop)));
+
+        });
+        tuple.groupBy(board);
+
+        //page 처리
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
+
+        long count = tuple.fetchCount();
+
+        log.info("COUNT: " +count);
+
+        return new PageImpl<Object[]>(
+                result.stream().map(t -> t.toArray()).collect(Collectors.toList()),
+                pageable,
+                count);
+    }
+}
