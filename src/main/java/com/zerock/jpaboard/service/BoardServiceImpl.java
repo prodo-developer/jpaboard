156d7a533b1497366d7f2c75b722aba666cfb395
prodo-developer <wjdals300@gmail.com>
2021-08-14 01:51:36 +0900

    feat : 게시판 검색조건 추가

--- src/main/java/com/zerock/jpaboard/service/BoardServiceImpl.java
+++ src/main/java/com/zerock/jpaboard/service/BoardServiceImpl.java
@@ -44,7 +44,14 @@ public class BoardServiceImpl implements BoardService {
 
         Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board) en[0],(Member)en[1],(Long)en[2]));
 
-        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+//      내림차순
+//      Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+//      검색조건 & 내림차순
+        Page<Object[]> result = repository.searchPage(
+                pageRequestDTO.getType(),
+                pageRequestDTO.getKeyword(),
+                pageRequestDTO.getPageable(Sort.by("bno").descending())
+        );
 
         return new PageResultDTO<>(result, fn);
     }
