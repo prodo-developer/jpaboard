a65f49838a060b96e9ddf0756941cec5b92617b7
prodo-developer <wjdals300@gmail.com>
2021-08-14 17:20:23 +0900

    refactor : 게시글 기준으로 댓글 목록 가져오기

--- src/main/java/com/zerock/jpaboard/repository/ReplyRepository.java
+++ src/main/java/com/zerock/jpaboard/repository/ReplyRepository.java
@@ -1,10 +1,13 @@
 package com.zerock.jpaboard.repository;
 
+import com.zerock.jpaboard.entity.Board;
 import com.zerock.jpaboard.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Java 8 이상을 사용하는 경우 오류 메시지에 제공된 다른 옵션을 적용 할 수 있습니다.
@@ -16,8 +19,12 @@ import org.springframework.data.repository.query.Param;
  * 아직까지는 이 옵션없이 사용하는 경우가 많으므로 @Param은 꼭 사용해주세요 :)
  */
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
+    
+    //Board 삭제시에 댓글들 삭제
     @Modifying
     @Query("delete from Reply r where r.board.bno =:bno ")
     void deleteByBno(@Param("bno") Long bno);
+
+    //게시물로 댓글 목록 가져오기
+    List<Reply> getRepliesByBoardOrderByRno(Board board);
 }
